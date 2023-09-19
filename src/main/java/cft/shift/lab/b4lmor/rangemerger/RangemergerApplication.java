4eeba0e06687e35b53ff9ea925a44fa23f2b0101
b4lmor <artem.lisitsin.02@mail.ru>
2023-09-19 23:14:45 +0700

    initial commit

---
+++ src/main/java/cft/shift/lab/b4lmor/rangemerger/RangemergerApplication.java
@@ -0,0 +1,13 @@
+package cft.shift.lab.b4lmor.rangemerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangemergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangemergerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cft/shift/lab/b4lmor/rangemerger/RangemergerApplicationTests.java
@@ -0,0 +1,13 @@
+package cft.shift.lab.b4lmor.rangemerger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangemergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
